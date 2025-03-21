64d41918fe8f7cf8977e151bd9a51628648c48c0
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-03-21 19:20:23 -0400

    Remove garbanzo app

--- apps/web-ui/README.md
+++
@@ -1,6 +0,0 @@
-# Tanglenode
-
-Get started with:
-
-- `bun i` to install deps
-- `bun run dev` to start the development server
--- apps/web-ui/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h2>Home</h2>
-      <p>This is the home page.</p>
-    </div>
-  );
-}
