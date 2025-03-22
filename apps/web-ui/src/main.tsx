4a756b89078a5dbb893ce8af7ab1e7910141a3ae
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-03-21 22:34:26 -0400

    Init fresh app

---
+++ apps/web-ui/src/main.tsx
@@ -0,0 +1,17 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.tsx";
+
+window.React2 = React;
+console.log(window.React1 === window.React2);
+
+// biome-ignore lint/style/noNonNullAssertion: <explanation>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
